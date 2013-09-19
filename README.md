68f2f1171e89187c1a75576e13fb40c5dd1b2075
hjpotter92 <hjpotter92@iitkgp.ac.in>
2013-09-19 18:35:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Lua Query Builder
+=================
+
+LuaQuB is a small query builder module for Lua. I am building this because I'm tired of typing similar queries over and over as strings.
---
+++ src/qublua.lua
@@ -0,0 +1,102 @@
+local luaqub = {}
+
+luaqub.__index = luaqub
+
+local function Trim( sInput )
+	return sInput:gsub( "^[%s]*(.-)[%s]*$", "%1" )
+end
+
+local function ParseString( sInput )
+	local tReturn = {}
+	for sMatch in sInput:gmatch( "[^,]+" ) do
+		table.insert( tReturn, Trim(sMatch) )
+	end
+	return tReturn
+end
+
+local function ParseTable( tInput )
+	local tReturn = {}
+	for Key, Value in pairs( tInput ) do
+		if tonumber( Key ) then
+			table.insert( tReturn, Trim(Value) )
+		else
+			table.insert( tReturn, ("%s AS %s"):format(Trim(Key), Trim(Value)) )
+		end
+	end
+	return tReturn
+end
+
+function luaqub:select( cols )
+	if not cols then
+		error( "Argument to select function expected" )
+		return false
+	end
+	if type( cols ) == "string" then
+		if Trim( cols ) = "*" then
+			self._select = { '*' }
+		else
+			self._select, cols = ParseString( cols ), nil
+		end
+	elseif type( cols ) == "table" then
+		self._select, cols = ParseTable( cols ), nil
+	end
+	self._flag = "select"
+	return self
+end
+
+function luaqub:from( tbls )
+	if not tbls then
+		error( "Argument to from function expected" )
+		return false
+	end
+	if type( tbls ) == "string" then
+		self._from, tbls = ParseString( tbls ), nil
+	elseif type( tbls ) == "table" then
+		self._from, tbls = ParseTable( tbls ), nil
+	end
+	return self
+end
+
+function luaqub:where( clauses )
+	if not clauses then
+		error( "Matching clauses to where function expected" )
+		return false
+	end
+	if type( tbls ) == "string" then
+		self._where, clauses = { clauses }, nil
+	elseif type( tbls ) == "table" then
+		self._where, clauses = clauses, nil
+	end
+	return self
+end
+
+function luaqub:limit( lim, off )
+	if not tonumber( lim ) then
+		error( "Limit argument should be a number" )
+		return false
+	end
+	if off then
+		if not tonumber( off ) then
+			error( "Offset argument should be a number" )
+			return false
+		end
+		self._offset = tonumber( off )
+	end
+	self._limit = tonumber( lim )
+	return self
+end
+
+function luaqub.new()
+	local tNew = {
+		_select = {},
+		_from = {},
+		_where = {},
+		_limit = 0,
+		_offset = 0,
+		_flag = '',
+	}
+	setmetatable( tNew, luaqub )
+	return tNew
+end
+
+return luaqub
